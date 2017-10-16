ec5ce21e27f154a7be7a64f0c4e0ada2c3a5f419
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-16 13:08:44 +0200

    finished exercise 3: add new task to to-do list based on user input

--- js2Test.js
+++ js2Test.js
@@ -1,12 +1,9 @@
-let imgs = document.querySelectorAll("li img");
-imgs.forEach(changeClassWithClick);
-function changeClassWithClick(img){
-    img.addEventListener('click', fadeOutAndCount);
-    function fadeOutAndCount(evt){
-        evt.target.classList.add("fade");
-        let nrOfFadeOutBall = document.querySelectorAll(".fade").length;
-        if(nrOfFadeOutBall == 3){
-            document.querySelector('audio').play();
-        }
-    }
+let input = document.querySelector('input');
+let list = document.querySelector('ul');
+let addTaskButton = document.querySelector('button');
+addTaskButton.addEventListener('click', addTask);
+function addTask(){
+    let addNewTask = document.createElement('li');
+    addNewTask.textContent = input.value;
+    list.appendChild(addNewTask);
 }
