cc09b7daa94ca759cf552b79ddedb4b150ee9fed
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-16 10:54:51 +0200

    finished 2nd exercise: fadeout element by click, then when the number of faded out elements reaches 3, trigger another event

--- js2Test.js
+++ js2Test.js
@@ -1,8 +1,12 @@
-let lis = document.querySelectorAll("li");
-lis.forEach(changeClassWithClick);
-function changeClassWithClick(li){
-    li.addEventListener('click', toggleClass);
-    function toggleClass(evt){
-        evt.target.classList.toggle("del");
+let imgs = document.querySelectorAll("li img");
+imgs.forEach(changeClassWithClick);
+function changeClassWithClick(img){
+    img.addEventListener('click', fadeOutAndCount);
+    function fadeOutAndCount(evt){
+        evt.target.classList.add("fade");
+        let nrOfFadeOutBall = document.querySelectorAll(".fade").length;
+        if(nrOfFadeOutBall == 3){
+            document.querySelector('audio').play();
+        }
     }
 }
