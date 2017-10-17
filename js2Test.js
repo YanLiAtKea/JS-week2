0b1964e960fa2b3507bb07a3b122cd1e76974a8b
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-17 10:33:23 +0200

    see comment!!

--- js2Test.js
+++ js2Test.js
@@ -3,6 +3,5 @@ img.addEventListener('click', changePos);
 function changePos(){
     let x = Math.random()*90;
     let y = Math.random()*90;
-    img.style.transform = "translate(" + x +"vw, " + y + "vh)";
-    console.log(x,y);
+    img.style.transform = "translate(" + x +"vw, " + y + "vh)"; // don't add ; for CSS in this line, because JS will add ; automaticly, so if add self, CSS will have 2; and breaks
 }
